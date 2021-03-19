faa4d8db6ad968558bc92887a67336192a8d9b6b
Majo Ledesma <55170175+majoledesma@users.noreply.github.com>
2021-03-19 15:47:42 -0300

    Add files via upload

---
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
+        playSound(soundName: sender.currentTitle!)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+}
